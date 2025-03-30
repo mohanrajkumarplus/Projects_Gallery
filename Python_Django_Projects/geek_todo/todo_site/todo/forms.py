83d1601e00721575a334430f0fcf7f55cbaa5a41
Mohan Raj Kumar V A <mohanrajkumarplus@gmail.com>
2025-03-30 16:28:01 +0530

    category addition
    category addition

--- Python_Django_Projects/geek_todo/todo_site/todo/forms.py
+++ Python_Django_Projects/geek_todo/todo_site/todo/forms.py
@@ -5,3 +5,9 @@ class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = "__all__"
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
+            'details': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Enter details'}),
+            'date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
--- Python_Django_Projects/geek_todo/todo_site/todo/models.py
+++ Python_Django_Projects/geek_todo/todo_site/todo/models.py
@@ -3,9 +3,17 @@ from django.utils import timezone
 # Create your models here.
 
 class Todo(models.Model):
+    CATEGORY_CHOICES = [
+        ('work', 'work'),
+        ('personal','pesonal'),
+        ('urgent','urgent'),
+        ('other','other'),
+    ]
+
     title = models.CharField(max_length=100)
     details = models.TextField()
     date = models.DateTimeField(default=timezone.now)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES,default='work')
 
     def __str__(self):
         return self.title
